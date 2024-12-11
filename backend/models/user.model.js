ec355c539ee683d5508b3b24236d97fb78cb8621
ArnavUpadhyay7 <arnavupadhyay7@gmail.com>
2024-12-12 02:02:49 +0530

    added user model

---
+++ backend/models/user.model.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const userSchema = new mongoose.Schema({
+    fullname: {
+        firstname: {
+            type: String,
+            required: true,
+            minlength: [3, 'First name must have atleast 3 letters']
+        },
+        lastname: {
+            type: String,
+            minlength: [3, 'Last name must have atleast 3 letters']
+        }
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: [5, 'Email must have atleast 3 letters']
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false
+    },
+    socketId: {
+        type: String,
+    }
+});
+
+userSchema.methods.generateAuthToken = function(){
+    const token = jwt.sign({_id: this.id}, process.env.JWT_SECRET);
+    return token;
+}
+
+userSchema.methods.comparePassword = async function(password){
+    return await bcrypt.compare(password, this.password);
+}
+
+userSchema.static.hashPassword = async function(password){
+    return await bcrypt.hash(password, 10);
+}
+
+const userModel = mongoose.model('user', userSchema);
+
+module.exports = userModel;
